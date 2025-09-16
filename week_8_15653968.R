772fd6fb99ee4470b4f05d399c0e4e44458c6834
Sara Loxton <saraloxton111@gmail.com>
2025-09-16 14:34:24 +1200

    Task 1 and Task 2 finished.

--- week_8_15653968.R
+++ week_8_15653968.R
@@ -6,9 +6,26 @@ library(dplyr)
 library(readr)
 library(viridis)
 
-
-ire_map <- leaflet() %>%
+#making the NZ map
+nz_map <- leaflet() %>%
   addTiles() %>%
-  setView(lng = -8.240473, lat = 53.419751, zoom = 6)
+  setView(lng = 174.7762, lat = -41.2865, zoom = 5)
 
-ire_map
+nz_map
+
+#Adding markers for citys in NZ, no dunners because they arent special enough
+nz_pop <- data.frame(
+  city = c("Auckland", "Wellington", "Christchurch"),
+  lat  = c(-36.8485, -41.2865, -43.5321),
+  lng  = c(174.7633, 174.7762, 172.6362),
+  population = c(1711130, 427516, 410423) 
+  #got the populations from https://worldpopulationreview.com/cities/new-zealand
+)
+
+#Adding the markers to the map
+nz_map_pop <- leaflet(nz_pop) %>%
+  addTiles() %>%
+  setView(lng = 174.7762, lat = -41.2865, zoom = 5) %>%
+  addMarkers(~lng, ~lat, popup = ~paste(city, "<br>Population:", population))
+
+nz_map_pop
