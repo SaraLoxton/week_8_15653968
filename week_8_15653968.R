77402b3f2ac4eff13427963dedc2ff5aa45e9f9e
Sara Loxton <saraloxton111@gmail.com>
2025-09-17 21:45:41 +1200

    Task 3 has been achieved!

--- week_8_15653968.R
+++ week_8_15653968.R
@@ -6,6 +6,8 @@ library(dplyr)
 library(readr)
 library(viridis)
 
+
+########################### Task One ######################
 #making the NZ map
 nz_map <- leaflet() %>%
   addTiles() %>%
@@ -13,7 +15,8 @@ nz_map <- leaflet() %>%
 
 nz_map
 
-#Adding markers for citys in NZ, no dunners because they arent special enough
+########################### Task Two & Three ##########################
+#Adding markers for citys in NZ, no dunners because they aren't special enough (I was only doing three like the example)
 nz_pop <- data.frame(
   city = c("Auckland", "Wellington", "Christchurch"),
   lat  = c(-36.8485, -41.2865, -43.5321),
@@ -24,8 +27,15 @@ nz_pop <- data.frame(
 
 #Adding the markers to the map
 nz_map_pop <- leaflet(nz_pop) %>%
-  addTiles() %>%
-  setView(lng = 174.7762, lat = -41.2865, zoom = 5) %>%
-  addMarkers(~lng, ~lat, popup = ~paste(city, "<br>Population:", population))
+  addProviderTiles("Esri.WorldImagery") %>%
+  setView(lng = 174.77557, lat = -41.28664, zoom = 5) %>%
+  addMarkers(~lng, ~lat, popup = ~paste(city, "<br>Population", population)) %>%
+  addHeatmap(lng = ~lng,
+             lat = ~lat,
+             intensity = ~population/500)
 
 nz_map_pop
+
+
+######################### Task Four ########################
+
